900a477c5e5a8383e5cd83bb902c5d60594b19ba
Vinícius de Matos Brandão <dtivini@gmail.com>
2025-10-11 20:44:02 -0300

    Send message through email added.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import os
-from datetime import datetime
+import smtplib
 from requests import Response
 from twilio.rest import Client
 from typing import Union
@@ -37,13 +37,26 @@ def main():
     sender: int = os.getenv('SENDER')
     receiver: int = os.getenv('RECEIVER')
 
-
+    # Enlgish: If you want the message to be sent only by SMS, use only code 42 through 47, and delete or comment out lines 49 through 59.
+    # Português: Caso queira que a mensagem seja apenas enviada por SMS, utilize apenas do código 42 até 47, e apague ou comente as linhas 49 até 59.
     client: Client = Client(ACCOUNT_SID, AUTH_TOKEN)
     client.messages.create(
         body=msg,
         from_=sender,
         to=receiver
     )
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+
+        EMAIL_SENDER: str = os.getenv('EMAIL_SENDER')
+        EMAIL_PASSWORD: str = os.getenv('PASSWORD') 
+        EMAIL_RECEIVER: str = os.getenv('EMAIL_RECEIVER')
+
+        connection.starttls()
+        connection.login(user=EMAIL_SENDER, password=EMAIL_PASSWORD)
+        connection.sendmail(from_addr=EMAIL_SENDER,
+                            to_addrs=EMAIL_RECEIVER,
+                            msg=f"Subject:{values['description']}\n\n{msg}".encode("utf-8"))
 
 if __name__ == "__main__":
     main()
